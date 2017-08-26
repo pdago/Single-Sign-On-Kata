8148b9c1d5248828969e437407297213e5b5e963
pdago <p.dago.casas@gmail.com>
2017-08-26 10:53:09 +0200

    added test for register with invalid credentials

--- Python/single_sign_on.py
+++ Python/single_sign_on.py
@@ -1,9 +1,24 @@
+class SessionStorage():
+	def is_valid(self, token):
+		pass
+
+	def register(self, token):
+		pass
+
+	def unregister(self):
+		pass
+	
 class SingleSignOnRegistry:
+	session_storage = None
+
+	def __init__(self, session_storage = SessionStorage()):
+		self.session_storage = session_storage
 
 	def register_new_session(self, credentials):
 		token = None
 		if credentials_are_valid(credentials):
 			token = generate_token()
+			self.session_storage.register(token)
 
 		return token
 
@@ -25,4 +40,5 @@ def credentials_are_valid(credentials):
 
 def generate_token():
 	pass
+
 
--- Python/test_single_sign_on_registry.py
+++ Python/test_single_sign_on_registry.py
@@ -1,7 +1,7 @@
 import unittest
 from mock import MagicMock
 import single_sign_on
-from single_sign_on import SingleSignOnRegistry, SSOToken
+from single_sign_on import SingleSignOnRegistry, SSOToken, SessionStorage
 
 class SsoTest(unittest.TestCase):
 	name = 'Earl'
@@ -11,14 +11,22 @@ class SsoTest(unittest.TestCase):
 	def test_register_new_session_for_valid_credentials(self):
 		single_sign_on.credentials_are_valid = MagicMock(return_value=True)
 		single_sign_on.generate_token = MagicMock(return_value=self.token)
+		SessionStorage.register = MagicMock()
 		sso_registry = SingleSignOnRegistry()
 		response_token = sso_registry.register_new_session(self.credentials)
 
 		single_sign_on.credentials_are_valid.assert_called_once_with(self.credentials)
 		single_sign_on.generate_token.assert_called_once_with()
+		SessionStorage.register.assert_called_once_with(self.token)
 
 		self.assertEquals(self.token, response_token)
 
+	def test_register_new_session_for_invalid_credentials(self):
+		single_sign_on.credentials_are_valid = MagicMock(return_value=False)
+		sso_registry = SingleSignOnRegistry()
+		response_token = sso_registry.register_new_session(self.credentials)
+
+		self.assertEquals(None, response_token)
 
 
 if __name__ == "__main__":
