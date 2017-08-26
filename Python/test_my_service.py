145039b8e08568c2316ba0502de532ccbf677539
pdago <p.dago.casas@gmail.com>
2017-08-26 09:46:07 +0200

    Added variables to test name and token

--- Python/test_my_service.py
+++ Python/test_my_service.py
@@ -3,11 +3,13 @@ import unittest
 from my_service import *
 
 class MyServiceTest(unittest.TestCase):
-
+    name = 'Earl'
+    token = 'e4r1_t0k3n'
     def test_invalidSSOTokenIsRejected(self):
-        service = MyService(None)
-        response = service.handle_request(Request("Foo", None))
-        self.assertNotEqual("hello Foo!", response.text)
+        SingleSignOnRegistry
+        service = MyService(SingleSignOnRegistry())
+        response = service.handle_request(Request(self.name, self.token))
+        self.assertEqual(None, response)
         
         
 if __name__ == "__main__":
