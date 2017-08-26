b97368d29313c92cc747b2334b728bbcabec5e39
pdago <p.dago.casas@gmail.com>
2017-08-26 09:54:54 +0200

    Added token for accepted name

--- Python/test_my_service.py
+++ Python/test_my_service.py
@@ -12,6 +12,13 @@ class MyServiceTest(unittest.TestCase):
         response = service.handle_request(Request(self.name, self.token))
         self.assertEqual(None, response)
         SingleSignOnRegistry.is_valid.assert_called_once_with(self.token)
-        
+
+    def test_validSSOTokenIsAccepted(self):
+        SingleSignOnRegistry.is_valid = MagicMock(return_value=True)
+        service = MyService(SingleSignOnRegistry())
+        response = service.handle_request(Request(self.name, self.token))
+        self.assertEqual('Hello ' + self.name + '!', response.text)
+        SingleSignOnRegistry.is_valid.assert_called_once_with(self.token)
+
 if __name__ == "__main__":
     unittest.main()
